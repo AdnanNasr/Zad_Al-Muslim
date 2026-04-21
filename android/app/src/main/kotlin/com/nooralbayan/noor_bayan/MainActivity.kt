f52bc4b93a8a2575de77d502b354939593528b82
AdnanNasr <zedxxxxx317@gmail.com>
2026-04-21 21:48:50 +0200

    update git android

--- android/app/src/main/kotlin/com/nooralbayan/noor_bayan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.nooralbayan.noor_bayan
-
-import com.ryanheise.audioservice.AudioServiceActivity
-
-class MainActivity : AudioServiceActivity()
--- android/app/src/main/kotlin/com/nooralbayan/noor_quran/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.nooralbayan.noor_quran
-
-import com.ryanheise.audioservice.AudioServiceActivity
-
-class MainActivity : AudioServiceActivity()
