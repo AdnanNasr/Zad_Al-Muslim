0d1dd8581622618d52a323ab00b259ba8ce60390
AdnanNasr <zedxxxxx317@gmail.com>
2026-04-21 21:49:27 +0200

    push new android update

---
+++ android/app/src/main/kotlin/com/nooralbayan/noor_bayan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nooralbayan.noor_bayan
+
+import com.ryanheise.audioservice.AudioServiceActivity
+
+class MainActivity : AudioServiceActivity()
---
+++ android/app/src/main/kotlin/com/nooralbayan/noor_quran/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nooralbayan.noor_quran
+
+import com.ryanheise.audioservice.AudioServiceActivity
+
+class MainActivity : AudioServiceActivity()
